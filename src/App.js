d95c1b60e65cfd50ccc546182c5e280c0d6b7ca2
Shehan  Shanaka <39334423+shehancshanaka@users.noreply.github.com>
2022-05-03 20:31:51 +0530

    install styled components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
 
 function App() {
   return (
-    <h1 style={{textAlign:"center"}}> IT's going awesome</h1>
+<>
+<h1> hellow</h1>
+</>
+
   );
 }
 
